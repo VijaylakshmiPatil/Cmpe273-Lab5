14b4ec853f4d6535a3178e13fa71a550aa9c7c4d
vijaylakshmipatil <vijaylakshmiptl@gmail.com>
2014-04-12 14:54:29 -0700

    First commit

---
+++ library/src/main/resources/assets/js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var items = $('td[title="status"]');
+    for( i=0; i<items.length; i++ ){
+            var status = items[i].innerHTML;
+            var isbn = items[i].id.slice("6");
+            var buttonID = "button#"+isbn;
+            if(status == "lost")
+                    {
+                            $(buttonID).attr("disabled",true);
+                    }
+            else{
+                    $(buttonID).removeAttr("disabled");
+            }
+    }
+    
+});
+
+$(":button").click(function() {
+    var isbn = this.id;
+    var status = "{{status}}";
+    //alert('About to report lost on ISBN ' + isbn);
+    if(confirm('Reporting a lost Book ' + isbn + '\nConfirm?')){
+    	$.ajax({
+        	url: "/library/v1/books/"+isbn+"?status=lost",
+        	type: "PUT",
+        	contentType: "application/json",
+        });
+	    window.location.reload(true);
+    }
+        
+});
